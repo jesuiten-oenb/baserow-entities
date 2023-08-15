bd33f33089b7f770bc4fee2a7380638ed5578fda
csae8092 <peter.andorfer@oeaw.ac.at>
2023-08-15 14:39:43 +0200

    script to create ms_desc xmls added

--- make_msdesc.py
+++ make_msdesc.py
@@ -4,16 +4,34 @@ import jinja2
 
 from acdh_tei_pyutils.tei import TeiReader
 
-from config import PROJECT_TITLE
+from config import PROJECT_TITLE, JSON_FOLDER
 
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./templates")
 templateEnv = jinja2.Environment(
     loader=templateLoader, trim_blocks=True, lstrip_blocks=True
 )
-template = templateEnv.get_template("ms_desc.xml")
+template = templateEnv.get_template("msdesc.xml")
 out_dir = os.path.join("data", "editions")
 
 os.makedirs(out_dir, exist_ok=True)
+
+with open(os.path.join(JSON_FOLDER, "msdesc.json"), "r") as f:
+    data = json.load(f)
+
+with open(os.path.join(JSON_FOLDER, "ms_item.json"), "r") as f:
+    ms_item_data = json.load(f)
+
+ms_item_data = [value for key, value in ms_item_data.items()]
+
+
+for key, context in data.items():
+    save_path = os.path.join(out_dir, f"{context['xml_id']}.xml")
+    context["project_title"] = PROJECT_TITLE
+    ms_item_list = list(filter(lambda x: x["msdesc"][0]["value"] == context["signatur"], ms_item_data))
+    context["ms_items"] = sorted(ms_item_list, key=lambda x: int(x["order_number"]))
+    xml_data = template.render(context)
+    doc = TeiReader(xml_data)
+    doc.tree_to_file(save_path)
 
 
